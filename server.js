d4dc07dc1b4c4280d8b7df456709bc8325a9375b
Viktoria <vik25@i.ua>
2023-05-23 14:15:51 +0200

    added server.js

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://Vika:sATf1Rn6mwez5dm8@cluster0.jbm8q9s.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
-// mongoose.set("strictQuery", true);
+const { DB_HOST } = require("./config");
 
 mongoose
   .connect(DB_HOST)
